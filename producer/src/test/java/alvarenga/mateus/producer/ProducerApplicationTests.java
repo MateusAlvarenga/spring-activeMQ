fab5ce4143a6ff7004438040c583d0d3fc4c522c
Mateus Alvarenga <mateus_alvarenga@protonmail.com>
2022-07-12 10:59:33 -0300

    init

---
+++ producer/src/test/java/alvarenga/mateus/producer/ProducerApplicationTests.java
@@ -0,0 +1,16 @@
+package alvarenga.mateus.producer;
+
+import jdk.jfr.Description;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+    @Test
+    @Description("should produce a message")
+    void produce() {
+
+    }
+
+}
