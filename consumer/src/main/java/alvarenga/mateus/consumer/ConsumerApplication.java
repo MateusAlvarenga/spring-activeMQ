096cd8ed6e9bc423ea1254aa6eefea6aaeb749e4
Mateus Alvarenga <mateus_alvarenga@protonmail.com>
2022-07-12 20:16:06 -0300

    refactor: refact

--- consumer/src/main/java/alvarenga/mateus/consumer/ConsumerApplication.java
+++ consumer/src/main/java/alvarenga/mateus/consumer/ConsumerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConsumerApplication {
 
     public static void main(String[] args) {
+        System.out.println("ConsumerApplication.main() 20:00");
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
